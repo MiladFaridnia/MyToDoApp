ee290b939a29c24134e8041cd07cbfdaebc37782
Milad <milad.faridnia@gmail.com>
2021-08-31 22:02:17 +0430

    Feat: Add TaskViewModel.kt and setup its dependencies

--- app/src/main/java/com/faridnia/mytodoapp/ui/task/TaskViewModel.kt
+++ app/src/main/java/com/faridnia/mytodoapp/ui/task/TaskViewModel.kt
@@ -2,9 +2,13 @@ package com.faridnia.mytodoapp.ui.task
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.faridnia.mytodoapp.data.TaskDao
 
 class TaskViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
+
+    val tasks = taskDao.getAllTasks().asLiveData()
+
 }
